af872635b1bf2d99c22c880274e7b2ba75a186c4
akam alam <arundelfootball23@gmail.com>
2020-09-24 06:09:12 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count = 0 
   value_index = nil 
   while count< array.length do 
-    if array[couont] == value_to_find
+    if array[count] == value_to_find
       value_index = count 
     end 
   count += 1 
